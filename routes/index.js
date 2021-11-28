2323975c204a48c9e8c93756d398dcf3f764723e
tuong404 <tuongka321@gmail.com>
2021-11-28 13:43:23 +0700

    fix(build): fix user delete route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const route = (app) => {
 
     app.post('/api/v1/login', controller.loginController)
     app.get('/api/v1/users/profile', controller.authentication, controller.getProfileController)
-    app.delete('/api/v1/users/1', controller.authentication, controller.deleteUserController)
+    app.delete('/api/v1/users/:id', controller.authentication, controller.deleteUserController)
 };
 
 module.exports = route;
